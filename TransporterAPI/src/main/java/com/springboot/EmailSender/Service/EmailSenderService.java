4b766310c17585e3db1d5ff6054426d2e35585c7
jeetd46 <115579363+jeetd46@users.noreply.github.com>
2023-08-29 11:58:03 +0530

    Revert "Updated Email Sending Feature"

--- TransporterAPI/src/main/java/com/springboot/EmailSender/Service/EmailSenderService.java
+++
@@ -1,8 +0,0 @@
-package com.springboot.EmailSender.Service;
-
-import com.springboot.EmailSender.Entities.EmailMessage.emailSentStatus;
-
-public interface EmailSenderService {
-	public emailSentStatus sendEmail(String receiverMailId, String senderName);
-	
-}
