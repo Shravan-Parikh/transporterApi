233f085caab943ccec0d19efef76ac4f677b3474
jayshree2419 <78597325+jayshree2419@users.noreply.github.com>
2023-11-17 13:51:03 +0530

    Update ShipperController.java

--- TransporterAPI/src/main/java/com/springboot/ShipperAPI/Controller/ShipperController.java
+++ TransporterAPI/src/main/java/com/springboot/ShipperAPI/Controller/ShipperController.java
@@ -52,12 +52,13 @@ public class ShipperController {
 		log.info("Get by shiperId Controller Started");
 		return new ResponseEntity<>(service.getOneShipper(shipperId),HttpStatus.OK);
 	}
-	
+
+	//endpoint to get transporter list and details by passing shipper id
 	@GetMapping("/transporterlist/{shipperId}")
 	private ResponseEntity<Object> getTransporterList(@PathVariable String shipperId) {
 		log.info("Get transporter list by shiperId Controller Started");
 		return new ResponseEntity<>(service.getTransporterList(shipperId),HttpStatus.OK);
-	}
+	} 
 
 
 
